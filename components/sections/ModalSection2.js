e078901d5161b9320933f05588eed0d5ba8b4f0a
Abishek William <abhishek@neurosphere.tech>
2024-06-08 19:24:45 +0530

    added new model

---
+++ components/sections/ModalSection2.js
@@ -0,0 +1,14 @@
+import ModelViewer2 from "../3d_model/new_model"
+
+export default function ModalSection2() {
+
+
+    return (
+        <>
+            {/* <section className="about" id="about-section"> */}
+               <ModelViewer2/>
+               {/* <h1>Helooooo</h1> */}
+            {/* </section> */}
+        </>
+    )
+}
